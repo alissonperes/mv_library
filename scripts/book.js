bfd67d0a5b8312f84b3d6fc0a130fc4108ba4f85
Alisson Peres <alissonpres.dll@gmail.com>
2020-02-17 18:06:46 -0300

    Add render function to book.js

--- scripts/book.js
+++ scripts/book.js
@@ -13,5 +13,29 @@ function addBookToLibrary(author, title, pages, read) {
 }
 
 addBookToLibrary("C.S. Lewis", "Narnia", 200, false);
-addBookToLibrary('Isaac Asmiov', 'I, Robot', 50, false);
-console.log(myLibrary);
+addBookToLibrary("Isaac Asmiov", "I, Robot", 50, false);
+
+let render = function(template, node) {
+  node.innerHTML += template;
+};
+
+let shelf = document.getElementById("shelf");
+
+for (var i = 0; i < myLibrary.length; i++) {
+  let newBook = `<div
+    class="col-sm-2 d-flex align-items-stretch card book text-white m-2"
+  >
+    <div
+      class="card-body text-center d-flex flex-column justify-content-between"
+    >
+      <h5 class="card-title col">${myLibrary[i].title}</h5>
+      <p class="card-text">by</p>
+      <p class="card-text">${myLibrary[i].author}</p>
+      <p class="card-text">${myLibrary[i].pages} pages</p>
+      <p class="card-text">${
+        myLibrary[i].read ? "Already read" : "Not read yet"
+      }</p>
+    </div>
+  </div>`;
+  render(newBook, shelf);
+}
