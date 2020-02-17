d55690d8723ff0d943c256b2279b370795adbb26
fedgut <fedgut@gmail.com>
2020-02-17 16:31:08 -0600

    Add index to book object

--- scripts/book.js
+++ scripts/book.js
@@ -1,41 +1,47 @@
 let myLibrary = [];
+let shelf = document.getElementById("shelf");
+let index = 0;
+
+let render = function(template, node) {
+  node.innerHTML += template;
+};
 
 function Book(author, title, pages, read) {
   this.author = author;
   this.title = title;
   this.pages = pages;
   this.read = read;
+  this.index = index++;
 }
 
 function addBookToLibrary(author, title, pages, read) {
   let book = new Book(author, title, pages, read);
-  myLibrary.push(book);
-}
-
-addBookToLibrary("C.S. Lewis", "Narnia", 200, false);
-addBookToLibrary("Isaac Asmiov", "I, Robot", 50, false);
-
-let render = function(template, node) {
-  node.innerHTML += template;
-};
-
-let shelf = document.getElementById("shelf");
-
-for (var i = 0; i < myLibrary.length; i++) {
   let newBook = `<div
     class="col-sm-2 d-flex align-items-stretch card book text-white m-2"
   >
     <div
       class="card-body text-center d-flex flex-column justify-content-between"
     >
-      <h5 class="card-title col">${myLibrary[i].title}</h5>
+      <h5 class="card-title col">${title}</h5>
       <p class="card-text">by</p>
-      <p class="card-text">${myLibrary[i].author}</p>
-      <p class="card-text">${myLibrary[i].pages} pages</p>
-      <p class="card-text">${
-        myLibrary[i].read ? "Already read" : "Not read yet"
-      }</p>
+      <p class="card-text">${author}</p>
+      <p class="card-text">${pages} pages</p>
+      <p class="card-text">${read ? "Already read" : "Not read yet"}</p>
     </div>
   </div>`;
   render(newBook, shelf);
+  myLibrary.push(book);
 }
+
+function createFromForm(form) {
+  author = form.author.value;
+  title = form.title.value;
+  pages = parseInt(form.pages.value);
+  read = form.read.checked;
+
+  addBookToLibrary(author, title, pages, read);
+  form.reset()
+}
+
+addBookToLibrary("C.S. Lewis", "Narnia", 200, false);
+addBookToLibrary("Isaac Asmiov", "I, Robot", 50, false);
