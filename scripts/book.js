bdefc7221eae86ddf210cc8fee0eb5c3cb6334fe
Alisson Peres <alissonpres.dll@gmail.com>
2020-02-17 20:01:21 -0300

    update book object

--- scripts/book.js
+++ scripts/book.js
@@ -12,35 +12,49 @@ function Book(author, title, pages, read) {
   this.pages = pages;
   this.read = read;
   this.index = index++;
+  this.toggleRead = function() {
+    this.read = !this.read;
+  };
 }
 
 function addBookToLibrary(author, title, pages, read) {
-  let book = new Book(author, title, pages, read);
-  let newBook = `<div
-    class="col-sm-2 d-flex align-items-stretch card book text-white m-2"
-  >
-    <div
-      class="card-body text-center d-flex flex-column justify-content-between"
+  if (author && title) {
+    let book = new Book(author, title, pages, read);
+    let newBook = `<div
+    class="col-sm-2 d-flex align-items-stretch card book text-white m-2" id="book${index}"
     >
-      <h5 class="card-title col">${title}</h5>
-      <p class="card-text">by</p>
-      <p class="card-text">${author}</p>
-      <p class="card-text">${pages} pages</p>
-      <p class="card-text">${read ? "Already read" : "Not read yet"}</p>
+    <div
+    class="card-body text-center d-flex flex-column justify-content-between"
+    >
+    <h5 class="card-title col">${title}</h5>
+    <p class="card-text">by</p>
+    <p class="card-text">${author}</p>
+    <p class="card-text">${pages} pages</p>
+    <p class="card-text">${read ? "Already read" : "Not read yet"}</p>
     </div>
-  </div>`;
-  render(newBook, shelf);
-  myLibrary.push(book);
+    <button class="btn btn-primary" onclick="removeBookFromLibrary(${index})"> Remove </button>
+    </div>`;
+    render(newBook, shelf);
+    myLibrary.push(book);
+  } else {
+    alert("Author and title needed to create book");
+  }
+}
+
+function removeBookFromLibrary(bookIndex) {
+  delete myLibrary[bookIndex - 1];
+  let removeBook = document.getElementById("book" + bookIndex);
+  removeBook.remove();
 }
 
 function createFromForm(form) {
   author = form.author.value;
   title = form.title.value;
-  pages = parseInt(form.pages.value);
+  pages = form.pages.value ? parseInt(form.pages.value) : "unknown";
   read = form.read.checked;
 
   addBookToLibrary(author, title, pages, read);
-  form.reset()
+  form.reset();
 }
 
 addBookToLibrary("C.S. Lewis", "Narnia", 200, false);
