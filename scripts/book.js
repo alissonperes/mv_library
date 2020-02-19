b2644fc9885b2b53edd42f4e80e417c50845eeac
fedgut <fedgut@gmail.com>
2020-02-19 09:57:47 -0600

    linting

--- scripts/book.js
+++ scripts/book.js
@@ -2,17 +2,26 @@ const set = (key, value) => localStorage.setItem(key, JSON.stringify(value));
 const get = key => localStorage.getItem(key);
 const remove = key => localStorage.removeItem(key);
 
-const shelf = document.getElementById('shelf');
-get('index') || set('index', 0);
+const shelf = document.getElementById("shelf");
+
+if (get("index") == null) {
+  set("index", 0);
+}
 
 const render = function render(template, node) {
   node.innerHTML += template;
 };
 
 function addBookToLibrary(book) {
+  const formError = document.getElementById("form_errors");
+  const formErrorMessage = document.getElementById('error_message');
+  console.log(formError);
   if (book.author && book.title) {
+    formError.removeChild(formErrorMessage);
     const newBook = `<div
-    class="col-sm-2 d-flex align-items-stretch card book text-white m-2" id="book${book.index}"
+    class="col-sm-2 d-flex align-items-stretch card book text-white m-2" id="book${
+      book.index
+    }"
     >
     <div
     class="card-body text-center d-flex flex-column justify-content-between"
@@ -22,17 +31,21 @@ function addBookToLibrary(book) {
     <p class="card-text">${book.author}</p>
     <p class="card-text">${book.pages} pages</p>
     <p class="card-text"><button id="btnBook${
-  book.index
-}" type="button" class="btn btn-secondary" onclick="toggleButton(${book.index})">${
-  book.read ? 'Read' : 'Unread'
-}</button></p>
+      book.index
+    }" type="button" class="btn btn-secondary" onclick="toggleButton(${
+      book.index
+    })">${book.read ? "Read" : "Unread"}</button></p>
     </div>
-    <button class="btn btn-primary" onclick="removeBookFromLibrary(${book.index})"> Remove </button>
+    <button class="btn btn-primary" onclick="removeBookFromLibrary(${
+      book.index
+    })"> Remove </button>
     </div>`;
 
     render(newBook, shelf);
   } else {
-    alert('Author and title are required');
+    formError.removeChild(formErrorMessage);
+    let errorMessage = "You need to fill Author and Title!";
+    render(errorMessage, formError);
   }
 }
 
@@ -45,7 +58,7 @@ function toggleButton(index) {
   parsed.read = parsed.read !== true;
   set(`book${index}`, parsed);
   const btn = document.getElementById(`btnBook${index}`);
-  btn.innerText = btn.innerText === 'Read' ? 'Unread' : 'Read';
+  btn.innerText = btn.innerText === "Read" ? "Unread" : "Read";
 }
 
 function Book(author, title, pages, read) {
@@ -53,8 +66,8 @@ function Book(author, title, pages, read) {
   this.title = title;
   this.pages = pages;
   this.read = read;
-  this.index = get('index');
-  set('index', parseInt(get('index'), 10) + 1);
+  this.index = get("index");
+  set("index", parseInt(get("index"), 10) + 1);
   this.toggleRead = function toggleRead() {
     this.read = !this.read;
     toggleButton(this.index);
@@ -64,17 +77,17 @@ function Book(author, title, pages, read) {
 function createFromForm(form) {
   const author = form.author.value;
   const title = form.title.value;
-  const pages = form.pages.value ? parseInt(form.pages.value, 10) : 'unknown';
+  const pages = form.pages.value ? parseInt(form.pages.value, 10) : "unknown";
   const read = form.read.checked;
   const book = new Book(author, title, pages, read);
-  set(`book${book.index}`, book);
   addBookToLibrary(book);
+  set(`book${book.index}`, book);
   form.reset();
 }
 
 function ready() {
   Object.keys(localStorage).map(x => {
-    if (typeof JSON.parse(localStorage[x]) === 'object') {
+    if (typeof JSON.parse(localStorage[x]) === "object") {
       const add = JSON.parse(localStorage[x]);
       addBookToLibrary(add);
     }
@@ -82,8 +95,8 @@ function ready() {
   });
 }
 
-if (document.readyState !== 'loading') {
+if (document.readyState !== "loading") {
   ready();
 } else {
-  document.addEventListener('DOMContentLoaded', ready);
+  document.addEventListener("DOMContentLoaded", ready);
 }
